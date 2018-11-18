9ed3d14312a6d1c770ac6ef2d552bb268374e636
Waseem Abbas <wajutt8@gmail.com>
2018-11-18 22:43:45 +0500

    Ajax form script moved to site.js Appointment creation screen ready

--- DesignExperts/wwwroot/js/site.js
+++ DesignExperts/wwwroot/js/site.js
@@ -1,4 +1,33 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+// ajax form events
+onBegin = function (res) {
+    $('#btn-save').attr('disabled', true);
+    $('body').loader('show');
+}
+onSuccess = function (res) {
+    $('#btn-save').attr('disabled', false);
+    $('body').loader('hide');
+
+    var titl, txt, typ;
+    if (res === "CRUD Success") {
+        titl = "Saved!";
+        txt = "New client saved successfully";
+        typ = "success";
+    } else {
+        titl = "Error!";
+        txt = "Error while processing your request";
+        typ = "warning";
+    }
+    swal({
+        title: titl,
+        text: txt,
+        type: typ,
+        buttonsStyling: false,
+        confirmButtonClass: 'btn btn-sm btn-light',
+        background: 'rgba(0, 0, 0, 0.96)'
+    });
+}
+
+//calander
